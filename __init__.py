a47d017ac07a1155eaf9b6aae3fadd28db517614
Matt Hancock <mhancock743@gmail.com>
2016-04-11 14:22:09 -0400

    Bulk initial.

---
+++ __init__.py
@@ -0,0 +1,102 @@
+"""
+--------------------------------------------------------
+Author: Matt Hancock, not.matt.hancock@gmail.com
+--------------------------------------------------------
+
+This python module implements an (ORM) object relational mapping to an sqlite database containing the annotation information from the XML files provided by the LIDC dataset. The point of this module is to make for easier data querying and to include functional aspects of the data models in addition to pure attribute information, e.g., computing nodule centroids from contour attribtues.
+
+The ORM is implemented using sqlalchemy. There are three data models:
+
+Scan, Annotation, and Contour
+
+The relationships are "one to many" for each model going left to right, i.e.,
+scans have many annotations and annotations have many contours.
+
+Example:
+
+
+For more information, see the model classes themselves.
+"""
+__version__ = 0.1
+
+# Hidden stuff.
+import os as _os
+from sqlalchemy import create_engine as _create_engine
+from sqlalchemy.orm import sessionmaker as _sessionmaker
+from ._Configuration import _Configuration
+
+_module_path = _os.path.dirname(_os.path.abspath(__file__))
+_engine      = _create_engine('sqlite:///'+_os.path.join(_module_path,'db','pylidc.sqlite'))
+_session     = _sessionmaker(bind=_engine)()
+
+# Public stuff.
+from .Scan import Scan
+from .Annotation import Annotation
+from .Annotation import _all_characteristics_ 
+from .Contour import Contour
+
+def query(*args):
+    """
+    Wraps the sqlalchemy session object. Some example usage:
+
+    >>> import pylidc as pl
+    >>> qu = pl.query(pl.Scan).filter(pl.Scan.resolution_z <= 1.)
+    >>> print qu.count()
+    >>> # => 97
+    >>> scan = qu.first()
+    >>> print len(scan.annotations)
+    >>> # => 11
+    >>> qu = pl.query(pl.Annotation).filter((pl.Annotation.malignancy > 3) & (pl.Annotation.spiculation < 3))
+    >>> print qu.count()
+    >>> # => 1083
+    >>> annotation = qu.first()
+    >>> print annotation.estimate_volume()
+    >>> # => 5230.33874999
+    """
+    return _session.query(*args)
+
+def get_path_to_dicom_files():
+    """
+    Get the root path to where the LIDC dicom data is stored. Note that the path is stored in an sqlite database, and so it will persist after it has been set.
+
+    Functions for visualization will look in:
+    
+        path_to_dicom_files/[tcia_id]/[study_instance_uid]/[series_instance_uid]/[number].dcm
+
+    Variables in brackets above will be set according to the pylidc.Ccan or pylidc.Annotation object that the function is called on. If the scans were downloaded using the TCIA download manager, the path should probably look like: `place/where/you/store/your/data/LIDC-IDRI`
+    """
+    cfg_obj = _session.query(_Configuration).filter(_Configuration.key == 'path_to_dicom_files').first()
+
+    # It should never be the case that the configuration is missing, but check for it and add it if missing anyway.
+    if cfg_obj is None:
+        _session.add(_Configuration(key='path_to_dicom_files',value=''))
+        _session.commit()
+        return ''
+    else:
+        return cfg_obj.value
+
+def set_path_to_dicom_files(path):
+    """
+    Set the root path to where the LIDC dicom data is stored. Note that the path is stored in an sqlite database, and so it will persist after it has been set.
+
+    path: string or None (default None)
+        If a string is provided, the path will be updated. If no argument is provided, the current root path is returned.
+
+    Functions for visualization will look in:
+    
+        path_to_dicom_files/[tcia_id]/[study_instance_uid]/[series_instance_uid]/[number].dcm
+
+    Variables in brackets above will be set according to the scan or annotation that the function is called on. If the scans were downloaded using the TCIA download manager, the path should probably look like: `place/where/you/store/your/data/LIDC-IDRI`
+    """
+    path = str(path)
+
+    cfg_obj = _session.query(_Configuration).filter(_Configuration.key == 'path_to_dicom_files').first()
+
+    # It should never be the case that the configuration is missing, but check for it and add it if missing anyway.
+    if cfg_obj is None:
+        cfg_obj = _Configuration(key='path_to_dicom_files',value=path)
+        _session.add(cfg_obj)
+    else:
+        cfg_obj.value = path
+    _session.commit()
+    print("Path updated successfully to `%s`"%path)
---
+++ db/_populate/populate.py
@@ -0,0 +1,165 @@
+import os, sys, re
+import numpy as np
+from xml.etree import ElementTree
+import dicom
+
+assert not os.path.exists(os.path.join(os.path.pardir, 'pylidc.db')), "`pylidc.db` already exists. Aborting."
+
+# Change these. The dicom path should end with `LIDC-IDRI`, and the xml path should end with `tcia-lidc-xml`.
+dicom_root_path = '/media/matt/fatty/Data/LIDC-IDRI'
+xml_root_path = '/home/matt/Downloads/tcia-lidc-xml'
+os.listdir(xml_root_path)
+
+
+
+
+#################################################################
+# Populate the database
+#################################################################
+
+# Import the module so we can use the models defined therein.
+import pylidc as pl
+
+# This line actually creates the database, `pylidc.db`.
+pl._Base.Base.metadata.create_all(pl._engine)
+
+# Add the dicom path as a configuration, so that the path to
+# dicom files persists across session. This is done so we can write
+# functions for visualizing annotations on top of CT data.
+pl._session.add(pl._Configuration(key='path_to_dicom_files', value=dicom_root_path))
+
+characteristic_names =\
+['subtlety',
+'internalStructure',
+'calcification',
+'sphericity',
+'margin',
+'lobulation',
+'spiculation',
+'texture',
+'malignancy']
+  
+xml_paths = np.load(os.path.join(os.path.curdir,'metadata','xml_paths.pkl'))
+init_ids  = np.load(os.path.join(os.path.curdir,'metadata','initial_release_ids.pkl'))
+tcia_ids  = np.load(os.path.join(os.path.curdir,'metadata','tcia_ids.pkl'))
+
+def load_xml(path):
+    with open(path,'r') as f:
+        # Parse the xml file into an element tree so we can find elements easy.
+        # ElementTree doesn't like the xmlns=... attribute for some reason, so
+        # we blank it out before creating the element tree from the file string.
+        xml = ElementTree.fromstring( re.sub(' xmlns="[^"]+"', '', f.read()) )
+    return xml
+
+assert os.path.exists(xml_root_path), "`xml_root_path` provided doesn't exist."
+
+def load_dcm(path):
+    with open(path,'r') as f:
+        img = dicom.read_file(f)
+    return img
+
+assert os.path.exists(dicom_root_path), "`dicom_root_path` provided doesn't exist."
+
+for count,xml_base_path in enumerate(xml_paths):
+    sys.stdout.write('Populating: %04d / 1018\r' % (count+1))
+    sys.stdout.flush()
+
+    # Create the xml tree.
+    xml_tree = load_xml(os.path.join(xml_root_path, xml_base_path+'.xml'))
+    study_instance_uid  = xml_tree.find('ResponseHeader').find('StudyInstanceUID').text
+    series_instance_uid = xml_tree.find('ResponseHeader').find('SeriesInstanceUid').text
+
+    # Load the dicom images into memory.
+    dcm_path = os.path.join(dicom_root_path, tcia_ids[xml_base_path], study_instance_uid, series_instance_uid) 
+    dcm_file_paths = os.listdir(dcm_path)
+    dcm_imgs = [load_dcm(os.path.join(dcm_path,dcm_file_path)) for dcm_file_path in dcm_file_paths if dcm_file_path.endswith('.dcm')]
+
+    # ##############################################
+    # Clean multiple z scans.
+    #
+    # Some scans contain multiple slices with the same `z` coordinate from the `ImagePositionPatient` tag.
+    # I'm making the arbitrary choice to take the slice with lesser `InstanceNumber` tag.
+    # This takes some work to accomplish...
+    zs    = [float(img.ImagePositionPatient[-1]) for img in dcm_imgs]
+    inums = [float(img.InstanceNumber) for img in dcm_imgs]
+    inds = range(len(zs))
+    while np.unique(zs).shape[0] != len(inds):
+        for i in inds:
+            for j in inds:
+                if i!=j and zs[i] == zs[j]:
+                    k = i if inums[i] > inums[j] else j
+                    inds.pop(inds.index(k))
+
+    # Prune the duplicates found in the loops above.
+    zs             = [zs[i] for i in range(len(zs)) if i in inds]
+    dcm_file_paths = [dcm_file_paths[i] for i in range(len(dcm_file_paths)) if i in inds]
+    dcm_imgs       = [dcm_imgs[i] for i in range(len(dcm_imgs)) if i in inds]
+
+    # Now sort everything by (now unique) ImagePositionPatient z coordinate.
+    sort_inds      = np.argsort(zs)
+    dcm_imgs       = [dcm_imgs[s] for s in sort_inds]
+    dcm_file_paths = [dcm_file_paths[s] for s in sort_inds]
+    zs             = [zs[s] for s in sort_inds]
+    # End multiple z clean.
+    # ##############################################
+
+    # This will be a handy field for the `Contour` object.
+    z_to_dcm_path = {float(dcm_imgs[i].ImagePositionPatient[-1]): dcm_file_paths[i] for i in range(len(dcm_imgs))}
+
+    # Create the scan object.
+    scan = pl.Scan(
+        study_instance_uid      = study_instance_uid,
+        series_instance_uid     = series_instance_uid,
+        patient_id              = tcia_ids[xml_base_path],
+        slice_thickness         = float(img.SliceThickness),
+        pixel_spacing           = float(img.PixelSpacing[0]),
+        is_from_initial         = (tcia_ids[xml_base_path] in init_ids),
+        contrast_used           = any(['Contrast' in d for d in dir(img)]),
+        sorted_dicom_file_names = ",".join(dcm_file_paths)
+    )
+
+    scan.annotations = []
+    # Now we add the annotations for nodules >3mm to the scan.
+    for session in xml_tree.findall('readingSession'):
+        for ann in session.findall('unblindedReadNodule'):
+            # If the nodule annotation doesn't contain a characteristics
+            # field, we continue, since we only care about the large nodule class.
+            chars = ann.find('characteristics')
+            if chars is None or len(chars)==0:
+                continue
+            # There are a few cases where a small nodule actually *does* have a characteristics field, but all the characteristics are 0 or blank.
+            bad_annotation = False
+            for c in chars:
+                if c.text == '0' or c.text == '' or c.text == None:
+                    bad_annotation = True
+                    break
+            if bad_annotation:
+                continue
+            
+            # Ok, now we know that this a large class nodule annotation.
+            annotation = pl.Annotation(_nodule_id=ann.find('noduleID').text)
+            for char_name in characteristic_names:
+                setattr(annotation, char_name, int(chars.find(char_name).text))
+
+            # Now we need to go one last level and add all the contours for each annotation.
+            annotation.contours = []
+            rois = ann.findall('roi')
+            for roi in rois:
+                # The coords line looks cryptic, but all we're doing is taking all the edgmaps and putting
+                # them into a single string with x,y points separated by a newline.
+                z_pos = float(roi.find('imageZposition').text) 
+                contour = pl.Contour(
+                    image_z_position = z_pos,
+                    dicom_file_name  = z_to_dcm_path[ z_pos ],
+                    inclusion        = roi.find('inclusion').text == 'TRUE',
+                    coords           = "\n".join([(em.find('xCoord').text+','+em.find('yCoord').text) for em in roi.findall('edgeMap')])
+                )
+                annotation.contours.append(contour)
+            scan.annotations.append(annotation)
+        pl._session.add(scan)
+print("")
+
+# Finally, we add the configuration option to set where the dicom files are stored.
+# This path is initialized to an empty string, and it's updated when later on when
+# the annotation viewing function is called.
+pl._session.commit()
