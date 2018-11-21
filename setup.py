72e1f0b1c7f7614933c0ecf788f8e43cf02ad068
matt <mhancock743@gmail.com>
2018-11-21 17:02:14 -0600

    Update setup file

--- setup.py
+++ setup.py
@@ -2,39 +2,42 @@ from os import path
 from setuptools import setup, find_packages
 from pylidc import __version__
 
-here = path.abspath(path.dirname(__file__))
 
-setup(
-    name='pylidc',
-    version=__version__,
-    description='A library for working with the LIDC dataset.',
-    long_description='',
-    url='https://github.com/pylidc/pylidc',
-    author='Matt Hancock',
-    author_email='not.matt.hancock@gmail.com',
-    license='MIT',
-    classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Healthcare Industry',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: MacOS',
-        'Operating System :: POSIX :: Linux',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Topic :: Database',
-        'Topic :: Scientific/Engineering :: Medical Science Apps.',
-        'Topic :: Scientific/Engineering :: Visualization'
-    ],
-    keywords='pylidc LIDC lung sql research',
-    packages=find_packages(exclude=['contrib', 'doc', 'tests*']),
-    install_requires=[
-        'sqlalchemy>=1.1.5', 'numpy>=1.12.0', 'scipy>=0.18.1',
-        'matplotlib>=2.0.0', 'pydicom>=1.0.0', 'scikit-image>=0.13'
-    ],
-    package_data={
-        'pylidc': ['pylidc.sqlite'],
-    }
-)
+def main():
+    setup(
+        name='pylidc',
+        version=__version__,
+        description='A library for working with the LIDC dataset.',
+        long_description='',
+        url='https://github.com/pylidc/pylidc',
+        author='Matt Hancock',
+        author_email='not.matt.hancock@gmail.com',
+        license='MIT',
+        classifiers=[
+            'Development Status :: 2 - Pre-Alpha',
+            'Intended Audience :: Healthcare Industry',
+            'Intended Audience :: Science/Research',
+            'License :: OSI Approved :: MIT License',
+            'Operating System :: MacOS',
+            'Operating System :: POSIX :: Linux',
+            'Programming Language :: Python :: 2',
+            'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.5',
+            'Topic :: Database',
+            'Topic :: Scientific/Engineering :: Medical Science Apps.',
+            'Topic :: Scientific/Engineering :: Visualization'
+        ],
+        keywords='pylidc LIDC lung sql research',
+        packages=find_packages(exclude=['contrib', 'doc', 'tests*']),
+        install_requires=[
+            'sqlalchemy>=1.1.5', 'numpy>=1.12.0', 'scipy>=0.18.1',
+            'matplotlib>=2.0.0', 'pydicom>=1.0.0', 'scikit-image>=0.13'
+        ],
+        package_data={
+            'pylidc': ['pylidc.sqlite'],
+        }
+    )
+
+if __name__ == '__main__':
+    main()
